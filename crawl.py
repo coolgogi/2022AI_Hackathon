47e458eeee0a2899dbf9b4e83a83d08438c61221
coolgogi <suhyun926@naver.com>
2022-01-20 15:40:00 +0900

    dog, rabbit .jpg

--- crawl.py
+++ crawl.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download
  
 response = google_images_download.googleimagesdownload()
  
-arguments = {"keywords": "조혜련","limit":10,"print_urls":True, "format": "jpg"}   #creating list of arguments
+arguments = {"keywords": "문근영 얼굴 사진","limit":100,"print_urls":True, "format": "jpg"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)
